e8b8905965ace4a97b3f7130e632f5cfa8e41fdc
vishalBelwal <vishalbelwal52@gmail.com>
2025-04-28 21:32:27 +0530

    feat: Update JWT verification to use constant for secret

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,5 +1,7 @@
 import { WebSocketServer } from "ws"
 import jwt, { JwtPayload } from "jsonwebtoken"
+import { JWT_SECRET } from "@repo/backend-common/config"
+
 const wss = new WebSocketServer({ port: 8080 })
 
 wss.on("connection", function connection(ws, request) {
@@ -8,7 +10,7 @@ wss.on("connection", function connection(ws, request) {
 
   const queryParams = new URLSearchParams(url.split("?")[1]);
   const token = queryParams.get("token");
-  const decoded = jwt.verify(token as string, process.env.JWT_SIGNIN_SECRET as string);
+  const decoded = jwt.verify(token as string, JWT_SECRET as string);
 
   if (!decoded || !(decoded as JwtPayload).userId) {
     ws.close();
