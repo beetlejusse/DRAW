bdaf1ca44ee4c4ab8184b29bbf242bea162b793a
vishalBelwal <vishalbelwal52@gmail.com>
2025-07-09 19:53:30 +0530

    http-server backend

--- apps/http-server/src/routes/auth.route.ts
+++ apps/http-server/src/routes/auth.route.ts
@@ -1,10 +1,10 @@
 import { Request, Response, Router } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { createUserSchema, SignInSchema } from "@repo/zod-common/zodtypes";
 import { JWTSecret } from "@repo/backend-common/config";
+import { prismaClient } from "@repo/postgresdb/prismaClient";
 
-const authRoute: Router = Router()
+const authRoute: Router = Router();
 
 authRoute.post("/signup", async (req: Request, res: Response) => {
     // try {
@@ -47,15 +47,28 @@ authRoute.post("/signup", async (req: Request, res: Response) => {
 
     // }
 
-    const data = createUserSchema.safeParse(req.body);
-    if (!data.success) {
+    const parsedData = req.body
+    if (!parsedData.success) {
         res.status(400).json({ message: "Invalid input" });
         return;
     }
+    try {
 
-    res.json({
-        userId: "Hello i am user Id"
-    })
+        await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.email,
+                password: parsedData.data.password,
+                username: parsedData.data.userName
+            }
+        })
+
+        res.json({
+            userId: parsedData.data
+        })
+    } catch (error) {
+        console.error("Error during signup:", error);
+        res.status(500).json({ message: "Failed to create User" });
+    }
 })
 
 authRoute.post("/signin", async (req: Request, res: Response) => {
@@ -91,21 +104,21 @@ authRoute.post("/signin", async (req: Request, res: Response) => {
     //     res.status(500).json({ message: "Internal server error", error: error.message });
     // }
 
-    const data = SignInSchema.safeParse(req.body);
+    const data = req.body
     if (!data.success) {
         res.status(400).json({ message: "Invalid input" });
         return;
     }
 
     const userId = 1
-    
+
     if (!JWTSecret) {
         res.status(500).json({ message: "JWT secret is not configured" });
-        return ;
+        return;
     }
-    
+
     const token = jwt.sign({ userId }, JWTSecret);
-    
+
     res.json({ token });
 })
 
